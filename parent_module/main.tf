a07b0a5b917f7bb3b9b501c5372ec04596ddecc7
Your Name <you@example.com>
2025-07-19 21:00:08 +0530

    new rg_added

--- parent_module/main.tf
+++ parent_module/main.tf
@@ -10,6 +10,12 @@ module "rg-name1" {
   location = "eastus"
 
 }
+module "rg-name2" {
+  source   = "../Child_module/azurerm_resource_group"
+  name     = "raj-rg2"
+  location = "eastus"
+
+}
 
 module "virtual_network" {
     depends_on = [ module.rg-name ]
